ea472ca4b793c4be1ebb20f004af18dab7e909c4
Frytz <dacook1983@gmail.com>
2020-08-03 17:27:05 -0400

    mvp and some stretch completed

--- api/server.js
+++ api/server.js
@@ -1,14 +1,47 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require('express-session');
+const knexSessionStore = require('connect-session-knex')(session);
+
+const sessionConfig = {
+  name: "johnsession",
+  secret: "my secret",
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false,
+
+  store: new knexSessionStore({
+    knex: require('../database/db-config'),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearinterval: 1000 * 60 *60
+  })
+    
+
+  
+};
 
 const server = express();
 server.use(helmet());
 server.use(express.json());
-
 server.use(cors());
+server.use(session(sessionConfig));
 
-server.get("/", (req, res) => {
+
+
+const usersRoute = require("../user/users_router");
+const Auth = require("../auth/auth_router")
+
+server.use("/api", usersRoute);
+server.use("/api", Auth)
+
+server.get("/api", (req, res) => {
   res.json({ api: "up" });
 });
 
